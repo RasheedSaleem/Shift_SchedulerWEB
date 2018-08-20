76773cf533c20772b86e507b113b466a8ca0fbd5
V0014109 <V0014109@MYIT00209>
2018-08-20 13:34:27 +0800

    Add project files.

---
+++ src/ShiftScheduler_Web/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShiftScheduler_Web.Helper;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShiftScheduler_Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeAPI _employeeApi = new EmployeeAPI();
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            List<EmployeeScheduleModel> lstEmpSch = new List<EmployeeScheduleModel>();
+            HttpClient client = _employeeApi.InitializeClient();
+            HttpResponseMessage res = await client.GetAsync("api/EmployeeSchedule");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                lstEmpSch = JsonConvert.DeserializeObject<List<EmployeeScheduleModel>>(result);
+            }
+            return View(lstEmpSch);
+        }
+    }
+}
---
+++ src/ShiftScheduler_Web/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ShiftScheduler_Web.Helper
+{
+    public class EmployeeAPI
+    {
+        private string _apiBaseURI = "http://localhost:55836";
+        public HttpClient InitializeClient()
+        {
+            var client = new HttpClient();
+            //Passing service base url  
+            client.BaseAddress = new Uri(_apiBaseURI);
+
+            client.DefaultRequestHeaders.Clear();
+            //Define request data format  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+
+    public class EmployeeScheduleModel
+    {
+        public Int16 Id { get; set; }
+        public DateTime Schedule_Date { get; set; }
+        public string Shift_Id { get; set; }
+        public string Emp_Id { get; set; }
+        public string Emp_Name { get; set; }
+    }
+}
